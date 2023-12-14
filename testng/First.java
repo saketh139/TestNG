ae1c6a7c07a20493fa4bc5fea33eae58a05cdb02
saketh139 <114804334+saketh139@users.noreply.github.com>
2023-12-14 22:01:23 +0530

    Add files via upload

---
+++ testng/First.java
@@ -0,0 +1,22 @@
+package com.testng;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class First {
+    @Test
+    public void TestGoogle(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        driver.findElement(By.name("q")).sendKeys("hyr tutorials", Keys.ENTER);
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+}
